709b67d4f345ff9edccdc1a43fc295c8790bbd7b
Jonathan Peters <j.j.p.peters@warwick.ac.uk>
2016-05-21 17:16:48 +0100

    More fixes, maybe

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,8 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function toggleMenu() {
     document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+}
+
+function closeMenu() {
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive", false);
 }
